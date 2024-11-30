d5b9efbac131f28154594f1e7e9e4d4c44c1d515
BinhAn14 <an100277@donga.edu.vn>
2024-11-30 07:17:40 +0700

    update

--- Nodejs/controllers/api_user_controller.mjs
+++ Nodejs/controllers/api_user_controller.mjs
@@ -1,24 +1,23 @@
-import User from "../../../models/user.mjs"; // Đảm bảo đường dẫn chính xác
+import User from '../nodejsda/B1/models/user.mjs'
+
 
 class ApiUserController {
     static async index(req, res) {
         try {
             let users = await User.find({});
             console.log(users);
-            res.status(200).json({ message: "Load dữ liệu thành công!!", data: users });
+            res
+                .status(200)
+                .json({ message: "Load du lieu thanh cong!!", data: users });
         } catch (error) {
             res.status(500).json({ message: error.message });
         }
     }
-
     static async show(req, res) {
         try {
             let id = req.params.id;
-            let user = await User.findById(id); // Đúng phương thức
-            if (!user) {
-                return res.status(404).json({ message: "Không tìm thấy người dùng" });
-            }
-            res.status(200).json({ message: "Lấy dữ liệu thành công", data: user }); // Đúng tên biến
+            let user = await User.findById(id);
+            res.status(200).json({ message: "Lấy dữ liệu thành công!!", data: user });
         } catch (error) {
             res.status(500).json({ message: error.message });
         }
@@ -27,7 +26,7 @@ class ApiUserController {
         try {
             let id = req.params.id;
             let data = await User.deleteOne({ _id: id });
-            res.status(200).json({ message: "Xóa thành công!!", data });
+            res.status(200).json({ message: "Xoa liệu thành công!!", data });
         } catch (error) {
             res.status(500).json({ message: error.message });
         }
@@ -35,8 +34,8 @@ class ApiUserController {
     static async create(req, res) {
         try {
             let { name, email, workExperience, age } = req.body;
-            let user = await User.create({ name, emailm, workExperience, age });
-            res.status(200).json({ message: "Tạo mới thành công!!", user });
+            let user = await User.create({ name, email, workExperience, age });
+            res.status(200).json({ message: "Tao Du liệu thành công!!", user });
         } catch (error) {
             res.status(500).json({ message: error.message });
         }
